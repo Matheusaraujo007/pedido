c6eb2826f3ac4aa83c04ea17962d8a190b1d6a47
Matheusaraujo007 <159270260+Matheusaraujo007@users.noreply.github.com>
2025-08-14 15:07:09 -0300

    Add files via upload

--- api/pedidos.js
+++ api/pedidos.js
@@ -1,22 +1,26 @@
 import { MongoClient } from "mongodb";
 
-const uri = process.env.MONGO_URI; // coloque no Vercel Environment Variables
 let client;
 
 export default async function handler(req, res) {
   if (req.method === "POST") {
     try {
-      client = client || new MongoClient(uri);
+      // Conectar ao MongoDB
+      client = client || new MongoClient("mongodb+srv://matheusaraujo85461:zaUHqFlqVHkMHr4W@cluster0.mongodb.net/pedidos?retryWrites=true&w=majority");
       await client.connect();
-      const db = client.db("meusPedidos");
-      const pedidosCollection = db.collection("pedidos");
+
+      const db = client.db("pedidos"); // nome do DB
+      const pedidosCollection = db.collection("pedidos"); // nome da collection
+
       const pedido = req.body;
 
+      // Inserir no MongoDB
       await pedidosCollection.insertOne(pedido);
-      res.status(200).json({ message: "Pedido salvo com sucesso!" });
+
+      res.status(200).json({ message: "✅ Pedido salvo com sucesso!" });
     } catch (error) {
       console.error(error);
-      res.status(500).json({ error: "Erro ao salvar pedido no MongoDB" });
+      res.status(500).json({ error: error.message });
     }
   } else {
     res.status(405).json({ error: "Método não permitido" });
