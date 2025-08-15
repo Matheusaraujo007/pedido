f44e347db7c601ce84f8261b0f925e68942ee226
Matheusaraujo007 <159270260+Matheusaraujo007@users.noreply.github.com>
2025-08-15 14:37:42 -0300

    Add files via upload

--- api/pedidos.js
+++ api/pedidos.js
@@ -22,12 +22,12 @@ export default async function handler(req, res) {
 
       await client.query(
         `INSERT INTO pedidos 
-          (nome_cliente, telefone_cliente, vendedor, itens, valor_total, valor_recebido, data_pedido, data_entrega, status)
+          (vendedor, nome_cliente, telefone_cliente, itens, valor_total, valor_recebido, data_pedido, data_entrega, status)
          VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)`,
         [
+          vendedor,
           nomeCliente,
           telefoneCliente,
-          vendedor,
           JSON.stringify(itens),
           valorTotal,
           valorRecebido || 0,
